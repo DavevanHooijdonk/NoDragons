f993061870ef7898de21bd7025319e73b8563b2b
Richard Dijk <microvirus.multiplying@gmail.com>
2016-01-09 01:40:26 +0100

    Creator controller: Added mock monster template for testing.

--- nodragons-frontend/js/pages/monster/creatorController.js
+++ nodragons-frontend/js/pages/monster/creatorController.js
@@ -1,2 +1,37 @@
 angular.module('noDragons').controller('creatorController', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+    // Mock object
+    $scope.monster =
+    {
+        "name": "Peter",
+        "id": 5,
+        "rank": "BOSS",
+        "stats": {
+            "maximumHitpoints": 10,
+            "zoneOfControl": 2,
+            "movementSpeed": 4,
+            "physicalResistance": 0,
+            "magicalResistance": 0,
+            "mentalResistance": 0
+        },
+        "passiveAbilities": [
+        {
+            "name": "Banana Passive",
+            "id": 1,
+            "effect": {
+                "effectDescription": "Wield the Power of Bananas"
+            }
+        }
+        ],
+        "activeAbilities": [
+        {
+            "name": "Banana Attack Strike Move Ability",
+            "id": 2,
+            "minimumDamage": 12,
+            "maximumDamage": 14,
+            "effect": {
+                "effectDescription": "Flying banana pain"
+            }
+        }
+        ]
+    };
 }]);
