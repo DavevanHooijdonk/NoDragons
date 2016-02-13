2befb9a782c3964987a736255528f486dbec75af
Richard Dijk <microvirus.multiplying@gmail.com>
2016-02-13 14:42:20 +0100

    Monster frontend: Added Create and Delete buttons for monster templates. Removed 'Monster Creator' nav button.

--- nodragons-frontend/js/pages/monster/overviewController.js
+++ nodragons-frontend/js/pages/monster/overviewController.js
@@ -1,4 +1,6 @@
-angular.module('noDragons').controller('overviewController', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+angular.module('noDragons').controller('overviewController', ['$scope', '$location', '$http', '$window', function ($scope, $location, $http, $window) {
+    $scope.serverActionInProgress = false;
+
     $http.get('/allmonstertemplates').then(function (data){
         $scope.templates = data.data;
     });
@@ -6,5 +8,21 @@ angular.module('noDragons').controller('overviewController', ['$scope', '$locati
     $scope.editMonster = function (id)
     {
         $location.path("/monstereditor/" + id);
+    }
+
+    $scope.createMonster = function ()
+    {
+        $scope.serverActionInProgress = true;
+        $http.post('/createmonstertemplate').then(
+            function (result)
+            {
+                $location.path('/monstereditor/' + result.data);
+            },
+            function (fail)
+            {
+                $scope.serverActionInProgress = false;
+                $window.alert("Failed to create new monster. Server error: " + fail.status + " " + fail.statusText);
+            }
+        );
     }
 }]);
